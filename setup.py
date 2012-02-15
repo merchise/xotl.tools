4019a9da07699ef0b2ab672d1fa9a8596caea104
Manuel Vázquez Acosta <mva.led@gmail.com>
2012-02-15 12:51:08 -0500

    Includes package stuff

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version = '1.0.0'
+
+setup(name='xoutil',
+      version=version,
+      description="Collection of usefull algorithms and other very disparate stuff",
+      long_description='',
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Merchise Autrement',
+      author_email='merchise.h8@gmail.com',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', ]),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        # 'zope.interface',
+      ],
+      entry_points="""
+      """,
+      )
