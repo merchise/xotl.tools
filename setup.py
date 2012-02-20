2a2565db22289fee615b8ad84e7dd97f63a28c54
Manuel Vázquez Acosta <mva.led@gmail.com>
2012-02-20 10:22:36 -0500

    Jumps to version 1.0.12

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
 
-version = '1.0.11'
+version = '1.0.12'
 
 setup(name='xoutil',
       version=version,
-      description="Collection of usefull algorithms and other very disparate stuff",
+      description="Collection of usefull algorithms and other very disparate "
+                  "stuff",
       long_description='',
       classifiers=[
-            # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # Get from http://pypi.python.org/pypi?%3Aaction=list_classifiers
             'Development Status :: 5 - Stable',
             'Intended Audience :: Developers',
             'License :: GPL',
