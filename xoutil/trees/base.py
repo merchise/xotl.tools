3810da5d681e1de68f349446f1fc73583d65e582
Manuel Vázquez Acosta <mva.led@gmail.com>
2012-02-23 08:23:30 -0500

    Extracts xotl.trees into xoutil.trees

---
+++ xoutil/trees/base.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------
+# xoutil.trees.base
+#----------------------------------------------------------------------
+# Copyright (c) 2011 Merchise Autrement
+# All rights reserved.
+#
+# Author: Medardo Rodriguez
+#
+# This is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License (GPL) as published by the
+# Free Software Foundation;  either version 2  of  the  License, or (at
+# your option) any later version.
+#       
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#       
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# Created on Feb 5, 2011
+
+'''
+doc
+'''
+
+
+from __future__ import (division as _py3_division, print_function as _py3_print, unicode_literals as _py3_unicode)
---
+++ xoutil/trees/ptt.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------
+# xoutil.trees.ptt
+#----------------------------------------------------------------------
+# Copyright (c) 2011 Merchise Autrement
+# All rights reserved.
+#
+# Author: Medardo Rodriguez
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#       
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#       
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# Created on Feb 5, 2011
+
+
+'''Xotl Preorder Tree Traversal.'''
+
+
+
+from __future__ import (division as _py3_division, print_function as _py3_print, unicode_literals as _py3_unicode)
+
