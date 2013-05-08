810a9eb384f2a07a0ca8bd671e83dbf17f5f2461
Manuel Vázquez Acosta <mva.led@gmail.com>
2013-05-08 16:48:10 -0400

    Updates copyright and license notices

--- xoutil/cli/app.py
+++ xoutil/cli/app.py
@@ -3,23 +3,11 @@
 #----------------------------------------------------------------------
 # xoutil.cli.app
 #----------------------------------------------------------------------
-# Copyright (c) 2013 Merchise Autrement
+# Copyright (c) 2013 Merchise Autrement and Contributors
 # All rights reserved.
 #
 # This is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License (GPL) as published by the
-# Free Software Foundation;  either version 2  of  the  License, or (at
-# your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# MA 02110-1301, USA.
+# the terms of the LICENCE attached in the distribution package.
 #
 # Created on 7 mai 2013
 
