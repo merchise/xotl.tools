856fb8bd7aaa778b0ef65f76224df16a63ea3c7b
Manuel Vázquez Acosta <mva.led@gmail.com>
2013-06-07 13:02:35 -0400

    Introduces `xoutil.string.input` which is compatible with between Py2 and py3k. TODO: This probably should be in `future`.

--- xoutil/string.py
+++ xoutil/string.py
@@ -338,3 +338,22 @@ def make_a10z(string):
        p13n
     '''
     return string[0] + str(len(string[1:-1])) + string[-1]
+
+if not _py3k:
+    input = lambda prompt=None: safe_decode(raw_input(prompt))
+    input.__doc__ = '''If the prompt argument is present, it is written to
+    standard output without a trailing newline. The function then reads a line
+    from input, converts it to a string (stripping a trailing newline), and
+    returns that. When EOF is read, EOFError is raised. Example::
+
+        >>> s = input('--> ')
+        --> Monty Python's Flying Circus
+        >>> s
+        "Monty Python's Flying Circus"
+
+    If the ``readline`` module was loaded, then :func:`input` will use it to
+    provide elaborate line editing and history features.
+
+    '''
+else:
+    from builtins import input
