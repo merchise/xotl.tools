55de95ec467ca8d505ab5af9b9008fab9616fec0
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-02-26 11:23:47 -0500

    Give attribution to the PSF for functools tests.

--- tests/test_functools2.py
+++ tests/test_functools2.py
@@ -1,3 +1,9 @@
+# Copied from Python 3.3 base code
+#
+# Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011,
+# 2012 Python Software Foundation.  All rights reserved.
+#
+
 from xoutil import functools
 import unittest
 from random import choice
