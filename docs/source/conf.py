62d648f00ad3bf21b3a037fc5c728cc9c8418a60
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-05-23 12:10:21 -0400

    Include `str2int` and `int2str` documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -281,7 +281,7 @@ texinfo_documents = [
 #texinfo_show_urls = 'footnote'
 
 
-# Example configuration for intersphinx: refer to the Python standard library.
+# Configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'py2': ('http://docs.python.org/2.7/', None),
     'py3': ('http://docs.python.org/3.4/', None),
