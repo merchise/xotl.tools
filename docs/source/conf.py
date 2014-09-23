234cc0195f17b3dcd01f10ac8f810b7388049a07
Manuel Vázquez Acosta <mva.led@gmail.com>
2014-09-23 11:11:21 -0400

    Fix the rtd theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
               'sphinx.ext.doctest',
               'sphinx.ext.viewcode',
-              'sphinx.ext.intersphinx',
+#              'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
               'sphinx.ext.coverage']
 
@@ -125,7 +125,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'pyramid'
+try:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except ImportError:
+    html_theme = 'pyramid'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
