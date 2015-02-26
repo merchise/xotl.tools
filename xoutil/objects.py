bcff2d79b432fac1af8a970ef12ff5008bae8c43
Medardo Rodriguez <med@merchise.org>
2015-02-25 21:30:28 -0500

    Import "metaclass" in "objects" from "six.meta".

--- xoutil/objects.py
+++ xoutil/objects.py
@@ -25,9 +25,9 @@ from __future__ import (division as _py3_division,
                         absolute_import)
 
 from xoutil import Unset
-from six import PY3 as _py3k, callable, string_types as str_base
+from six import callable, string_types as str_base
 from xoutil.deprecation import deprecated
-
+from .six.meta import metaclass
 
 __docstring_format__ = 'rst'
 
@@ -1152,129 +1152,6 @@ def extract_attrs(obj, *names, **kwargs):
         raise TypeError('Invalid keyword arguments for `extract_attrs`')
     getter = get_traverser(*names, default=default)
     return getter(obj)
-
-
-if _py3k:
-    from xoutil import _meta3
-    __m = _meta3
-else:
-    from xoutil import _meta2
-    __m = _meta2
-
-metaclass = __m.metaclass
-
-
-metaclass.__doc__ = '''Define the metaclass of a class.
-
-    .. versionadded:: 1.4.1
-
-    This function allows to define the metaclass of a class equally in Python
-    2 and 3.
-
-    Usage::
-
-       >>> class Meta(type):
-       ...   pass
-
-       >>> class Foobar(metaclass(Meta)):
-       ...   pass
-
-       >>> class Spam(metaclass(Meta), dict):
-       ...   pass
-
-       >>> type(Spam) is Meta
-       True
-
-       >>> Spam.__bases__ == (dict, )
-       True
-
-    .. versionadded:: 1.5.5 The `kwargs` keywords arguments with support for
-       ``__prepare__``.
-
-    Metaclasses are allowed to have a ``__prepare__`` classmethod to return
-    the namespace into which the body of the class should be evaluated.  See
-    :pep:`3115`.
-
-    .. warning:: The :pep:`3115` is not possible to implement in Python 2.7.
-
-       Despite our best efforts to have a truly compatible way of creating
-       meta classes in both Python 2.7 and 3.0+, there is an inescapable
-       difference in Python 2.7.  The :pep:`3115` states that ``__prepare__``
-       should be called before evaluating the body of the class.  This is not
-       possible in Python 2.7, since ``__new__`` already receives the
-       attributes collected in the body of the class.  So it's always too late
-       to call ``__prepare__`` at this point and the Python 2.7 interpreter
-       does not call it.
-
-       Our approach for Python 2.7 is calling it inside the ``__new__`` of a
-       "side" metaclass that is used for the base class returned.  This means
-       that ``__prepare__`` is called **only** for classes that use the
-       `metaclass`:func: directly.  In the following hierarchy::
-
-           class Meta(type):
-                @classmethod
-                def __prepare__(cls, name, bases, **kwargs):
-                    from xoutil.collections import OrderedDict
-                    return OrderedDict()
-
-           class Foo(metaclass(Meta)):
-                pass
-
-           class Bar(Foo):
-                pass
-
-       when creating the class ``Bar`` the ``__prepare__()`` class method is
-       not called in Python 2.7!
-
-    .. seealso:: `xoutil.types.prepare_class`:func: and
-       `xoutil.types.new_class`:func:.
-
-    .. warning::
-
-       You should always place your metaclass declaration *first* in the list
-       of bases. Doing otherwise triggers *twice* the metaclass' constructors
-       in Python 3.1 or less.
-
-       If your metaclass has some non-idempotent side-effect (such as
-       registration of classes), then this would lead to unwanted double
-       registration of the class::
-
-          >>> class BaseMeta(type):
-          ...     classes = []
-          ...     def __new__(cls, name, bases, attrs):
-          ...         res = super(BaseMeta, cls).__new__(cls, name, bases, attrs)
-          ...         cls.classes.append(res)   # <-- side effect
-          ...         return res
-
-          >>> class Base(metaclass(BaseMeta)):
-          ...     pass
-
-          >>> class SubType(BaseMeta):
-          ...     pass
-
-          >>> class Egg(metaclass(SubType), Base):   # <-- metaclass first
-          ...     pass
-
-          >>> Egg.__base__ is Base   # <-- but the base is Base
-          True
-
-          >>> len(BaseMeta.classes) == 2
-          True
-
-          >>> class Spam(Base, metaclass(SubType)):
-          ...     'Like "Egg" but it will be registered twice in Python 2.x.'
-
-       In this case the registration of Spam ocurred twice::
-
-          >>> BaseMeta.classes  # doctest: +SKIP
-          [<class Base>, <class Egg>, <class Spam>, <class Spam>]
-
-       Bases, however, are just fine::
-
-          >>> Spam.__bases__ == (Base, )
-          True
-
-'''
 
 
 def register_with(abc):
