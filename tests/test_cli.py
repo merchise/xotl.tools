e44ed929f8c8ae6f2a42c85dfc7cc9893b07be0e
Manuel Vázquez Acosta <manu@merchise.org>
2015-04-15 18:39:15 -0400

    Fix Command's repr issue.

---
+++ tests/test_cli.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# test_cli
+# ---------------------------------------------------------------------
+# Copyright (c) 2015 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2015-04-15
+
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+
+def test_regression_Command_repr():
+    from xoutil.cli import Command
+
+    class MyCommand(Command):
+        def run(self):
+            pass
+
+    cmd = MyCommand()
+    assert repr(cmd) != ''
