fb97374d180a8a31eedc201c1eef43a3d3035f32
Manuel Vázquez Acosta <manu@merchise.org>
2015-10-14 17:51:01 -0400

    Fix the `python setup.py test` command.

--- setup.py
+++ setup.py
@@ -15,46 +15,58 @@ if RELEASE_TAG != '':
 else:
     dev_classifier = 'Development Status :: 5 - Production/Stable'
 
-setup(name=project_name,
-      version=version,
-      description=("Collection of usefull algorithms and other very "
-                   "disparate stuff"),
-      long_description=open(
-          os.path.join(_current_dir, 'docs', 'readme.txt')).read(),
-      classifiers=[
-          # Get from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-          dev_classifier,
-          'Intended Audience :: Developers',
-          ('License :: OSI Approved :: '
-           'GNU General Public License v3 or later (GPLv3+)'),
-          'Operating System :: POSIX :: Linux',  # This is where we are
-                                                 # testing. Don't promise
-                                                 # anything else.
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Topic :: Software Development :: Libraries :: Python Modules',
+from setuptools.command.test import test as TestCommand
 
-      ],
-      keywords='',
-      author='Merchise',
-      # TODO: [taqchi] create these accounts
-      author_email='project+xoutil@merchise.com',
-      # TODO: Negotiate with Maykel Moya to obtain plain "Merchise" folder at
-      #       "https://github.com"
-      # TODO: [taqchi] manage these accounts
-      url='https://github.com/merchise/xoutil/',
-      license='GPLv3+',
-      test_suite='pytest.collect',
-      test_requires=['pytest'],
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # TODO: 'six>=1.5.0,<2', removed
-      ],
-      extras_require={
-          'extra': ['python-dateutil', ],
-      },
-      entry_points="""
-      """,
-      )
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(
+    name=project_name,
+    version=version,
+    description=("Collection of usefull algorithms and other very "
+                 "disparate stuff"),
+    long_description=open(
+        os.path.join(_current_dir, 'docs', 'readme.txt')).read(),
+    classifiers=[
+        # Get from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        dev_classifier,
+        'Intended Audience :: Developers',
+        ('License :: OSI Approved :: '
+         'GNU General Public License v3 or later (GPLv3+)'),
+        'Operating System :: POSIX :: Linux',  # This is where we are
+                                               # testing. Don't promise
+                                               # anything else.
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='',
+    author='Merchise',
+    # TODO: [taqchi] create these accounts
+    author_email='project+xoutil@merchise.com',
+    # TODO: Negotiate with Maykel Moya to obtain plain "Merchise" folder at
+    #       "https://github.com"
+    # TODO: [taqchi] manage these accounts
+    url='https://github.com/merchise/xoutil/',
+    license='GPLv3+',
+    test_requires=['pytest'],
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # TODO: 'six>=1.5.0,<2', removed
+    ],
+    extras_require={
+        'extra': ['python-dateutil', ],
+    },
+    cmdclass={'test': PyTest},
+)
