675b41b0fa044ef732717d2d56b0d4ca9ef2a1e9
Medardo Rodriguez <med@merchise.org>
2015-10-25 08:48:32 -0400

    Add a module for simple locks.

---
+++ xoutil/lock/__init__.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# xoutil.lock
+# ---------------------------------------------------------------------
+# Copyright (c) 2015 Merchise and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2015-10-24
+
+'''Synchronization lock tools.
+
+A lock object is a synchronization primitive.  Locks are used as Python
+Context Managers with only one method (`enter`) and one property (`locked`).
+
+The method `enter` is used with the Python ``with`` statement and the property
+`locked` is logically True or False depending if the lock is active or not.
+
+For example::
+
+  >>> from xoutil.lock import context_lock as ctx
+
+  >>> def get_lock_value():
+  ...     return 'Yes' if ctx.locked else 'No'
+
+  >>> with ctx.enter():
+  ...     one = get_lock_value()
+  >>> two = get_lock_value()
+  >>> (one, two)
+  ('Yes', 'No')
+
+
+Locks are implemented using module-property; this means that each time you
+import it, a different lock is returned::
+
+  >>> from xoutil.lock import context_lock as one
+  >>> from xoutil.lock import context_lock as two
+  >>> one is two
+  False
+
+There are two variants of locks in this module:
+
+- `context_lock`: Use an execution context, see `xoutil.context`:mod: module
+  for more information.
+
+- `thread_lock`: Use `threading`:mod: Python standard module.
+
+'''
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+
+from xoutil.modules import moduleproperty
+
+
+@moduleproperty
+def context_lock(self):
+    '''Allocate a lock based on Xoutil execution contexts.'''
+    from xoutil.objects import classproperty
+
+    class ContextLock(object):
+        '''A class representing the lock.
+
+        See `xoutil.lock`:mod: module for more information.
+
+        '''
+        def __new__(cls, *args, **kwargs):
+            msg = '"{}" could not be instanced.'.format(cls.__name__)
+            raise RuntimeError(msg)
+
+        @classmethod
+        def enter(cls, **kwargs):
+            '''Enter the context.'''
+            from xoutil.context import context
+            return context(cls, **kwargs)
+
+        @classproperty
+        def locked(cls):
+            from xoutil.context import context
+            return context[cls]
+
+    return ContextLock
+
+
+# @moduleproperty
+# def thread_lock(self):
+#     '''Allocate a lock based on `threading`:mod: Python standard module.'''
+#     return None    # TODO: Use `_lock` module
+
+del moduleproperty
