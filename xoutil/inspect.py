6bc4eff97a9ea3bc8e0be5e349f291aeff2d95d3
Medardo Rodriguez <med@merchise.org>
2015-10-31 19:24:35 -0400

    Remove future `unicode_literals` from `inspect` module.

--- xoutil/inspect.py
+++ xoutil/inspect.py
@@ -21,7 +21,6 @@ document all items here.  Refer to :mod:`inspect's <inspect>` documentation.
 
 from __future__ import (division as _py3_division,
                         print_function as _py3_print,
-                        unicode_literals as _py3_unicode,
                         absolute_import as _absolute_import)
 
 
