3cdfd73dc25a2a4b9de6d06d6c74bf38addd4d1b
Medardo Rodriguez <med@merchise.org>
2015-11-22 08:28:23 -0500

    Prioritize descriptor gets in `OpenDictMixin`.

--- xoutil/collections.py
+++ xoutil/collections.py
@@ -256,6 +256,13 @@ class OpenDictMixin(object):
                 raise AttributeError(msg % (type(self).__name__, name))
 
     def __setattr__(self, name, value):
+        cls = type(self)
+        desc = getattr(cls, name, None)
+        if desc is not None:    # Prioritize descriptors
+            try:
+                desc.__set__(self, value)
+            except:
+                pass
         key = (~self).get(name)
         if key:
             self[key] = value
@@ -311,9 +318,11 @@ class OpenDictMixin(object):
         is not possible.
 
         '''
+        from xoutil.keywords import suffix_kwd
         from xoutil.string import normalize_slug
         from xoutil.validators import is_valid_identifier
-        return key if is_valid_identifier(key) else normalize_slug(key, '_')
+        res = key if is_valid_identifier(key) else normalize_slug(key, '_')
+        return suffix_kwd(res)
 
 
 class SmartDictMixin(object):
