a42c9f08ba03e83e3d7fa2b0af7f20ace17f8e89
Manuel Vázquez Acosta <manu@merchise.org>
2015-11-21 19:37:23 -0500

    Add an `exec_` for Python 2/3.

--- xoutil/eight/__init__.py
+++ xoutil/eight/__init__.py
@@ -128,3 +128,20 @@ if _py3:
     from io import StringIO
 else:
     from StringIO import StringIO    # noqa
+
+
+if _py3:
+    exec_ = getattr(builtins, 'exec')  # noqa
+else:
+    def exec_(_code_, _globs_=None, _locs_=None):
+        """Execute code in a namespace."""
+        import sys
+        if _globs_ is None:
+            frame = sys._getframe(1)
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
+            del frame
+        elif _locs_ is None:
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
