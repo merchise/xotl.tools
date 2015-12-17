43baf89db66b4dc05bf1c693ef746fbf6ee1c6d3
Manuel Vázquez Acosta <manu@merchise.org>
2015-12-17 17:03:44 -0500

    Remove unused imports.

--- xoutil/eight/exceptions.py
+++ xoutil/eight/exceptions.py
@@ -28,10 +28,6 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-
-from . import _py3, exec_
-
-
 try:
     from exceptions import StandardError
 except ImportError:
