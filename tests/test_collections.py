32274cdf2f27302d7cdd9116d983447761ca77e3
Medardo Rodriguez <med@merchise.org>
2016-03-09 15:18:20 -0500

    Add tests for new `RankedDict` class.

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -106,8 +106,9 @@ def test_stacked_dict():
 
 # Backported from Python 3.3.0 standard library
 from xoutil.eight import _py3 as PY3
-from xoutil.collections import ChainMap, Counter, OrderedDict, Mapping
-from xoutil.collections import MutableMapping
+from xoutil.collections import ChainMap, Counter
+from xoutil.collections import OrderedDict, RankedDict
+from xoutil.collections import Mapping, MutableMapping
 import copy
 import pickle
 from random import randrange
@@ -766,6 +767,264 @@ class TestOrderedDict(unittest.TestCase):
                 raise Exception()
         items = [('a', 1), ('c', 3), ('b', 2)]
         self.assertEqual(list(MyOD(items).items()), items)
+
+
+class TestRankedDict(unittest.TestCase):
+    def test_init(self):
+        with self.assertRaises(TypeError):
+            # too many args
+            RankedDict([('a', 1), ('b', 2)], None)
+        pairs = [('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)]
+        # dict input
+        self.assertEqual(sorted(RankedDict(dict(pairs)).items()), pairs)
+        # kwds input
+        self.assertEqual(sorted(RankedDict(**dict(pairs)).items()), pairs)
+        # pairs input
+        self.assertEqual(list(RankedDict(pairs).items()), pairs)
+        # mixed input
+        self.assertNotEqual(list(RankedDict([('a', 1), ('b', 2), ('c', 9),
+                                             ('d', 4)], c=3, e=5).items()),
+                            pairs)
+
+        # Make sure that direct calls to __init__ do not clear previous
+        # contents
+        d = RankedDict([('a', 1), ('b', 2), ('c', 3), ('d', 44), ('e', 55)])
+        d.__init__([('f', 6), ('e', 5), ], d=4)
+        self.assertEqual(list(d.items()),
+                         [('a', 1), ('b', 2), ('c', 3), ('f', 6), ('e', 5),
+                          ('d', 4)])
+
+    def test_update(self):
+        with self.assertRaises(TypeError):
+            # too many args
+            RankedDict().update([('a', 1), ('b', 2)], None)
+        pairs = [('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)]
+        od = RankedDict()
+        od.update(dict(pairs))
+        # dict input
+        self.assertEqual(sorted(od.items()), pairs)
+        od = RankedDict()
+        od.update(**dict(pairs))
+        # kwds input
+        self.assertEqual(sorted(od.items()), pairs)
+        od = RankedDict()
+        od.update(pairs)
+        # pairs input
+        self.assertEqual(list(od.items()), pairs)
+        od = RankedDict()
+        od.update([('a', 1), ('b', 2), ('c', 9), ('d', 4)], c=3, e=5)
+        # mixed input
+        self.assertNotEqual(list(od.items()), pairs)
+
+        # XXX: Issue 9137: Named argument called 'other' or 'self'
+        # shouldn't be treated specially.
+        # od = RankedDict()
+        # od.update(self=23)
+        # self.assertEqual(list(od.items()), [('self', 23)])
+        # od = RankedDict()
+        # od.update(other={})
+        # self.assertEqual(list(od.items()), [('other', {})])
+        # od = RankedDict()
+        # od.update(red=5, blue=6, other=7, self=8)
+        # self.assertEqual(sorted(list(od.items())),
+        #                  [('blue', 6), ('other', 7), ('red', 5), ('self', 8)])
+        # end of XXX
+
+    def test_abc(self):
+        self.assert_(isinstance(RankedDict(), MutableMapping))
+        self.assertTrue(issubclass(RankedDict, MutableMapping))
+
+    def test_clear(self):
+        pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
+        shuffle(pairs)
+        od = RankedDict(pairs)
+        self.assertEqual(len(od), len(pairs))
+        od.clear()
+        self.assertEqual(len(od), 0)
+
+    def test_delitem(self):
+        pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
+        od = RankedDict(pairs)
+        del od['a']
+        self.assertNotIn('a', od)
+        with self.assertRaises(KeyError):
+            del od['a']
+        self.assertEqual(list(od.items()), pairs[:2] + pairs[3:])
+
+    def test_setitem(self):
+        od = RankedDict([('d', 1), ('b', 2), ('c', 3), ('a', 4), ('e', 5)])
+        od['c'] = 10           # existing element
+        od['f'] = 20           # new element
+        self.assertEqual(list(od.items()), [('d', 1), ('b', 2), ('a', 4),
+                                            ('e', 5), ('c', 10), ('f', 20)])
+
+    def test_iterators(self):
+        pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
+        shuffle(pairs)
+        od = RankedDict(pairs)
+        self.assertEqual(list(od), [t[0] for t in pairs])
+        self.assertEqual(list(od.keys()), [t[0] for t in pairs])
+        self.assertEqual(list(od.values()), [t[1] for t in pairs])
+        self.assertEqual(list(od.items()), pairs)
+        self.assertEqual(list(reversed(od)),
+                         [t[0] for t in reversed(pairs)])
+
+    def test_popitem(self):
+        pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
+        shuffle(pairs)
+        od = RankedDict(pairs)
+        while pairs:
+            self.assertEqual(od.popitem(), pairs.pop())
+        with self.assertRaises(KeyError):
+            od.popitem()
+        self.assertEqual(len(od), 0)
+
+    def test_pop(self):
+        pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
+        shuffle(pairs)
+        od = RankedDict(pairs)
+        shuffle(pairs)
+        while pairs:
+            k, v = pairs.pop()
+            self.assertEqual(od.pop(k), v)
+        with self.assertRaises(KeyError):
+            od.pop('xyz')
+        self.assertEqual(len(od), 0)
+        self.assertEqual(od.pop(k, 12345), 12345)
+
+        # make sure pop still works when __missing__ is defined
+        class Missing(RankedDict):
+            def __missing__(self, key):
+                return 0
+        m = Missing(a=1)
+        self.assertEqual(m.pop('b', 5), 5)
+        self.assertEqual(m.pop('a', 6), 1)
+        self.assertEqual(m.pop('a', 6), 6)
+        with self.assertRaises(KeyError):
+            m.pop('a')
+
+    def test_equality(self):
+        pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
+        shuffle(pairs)
+        od1 = RankedDict(pairs)
+        od2 = RankedDict(pairs)
+        # same order implies equality
+        self.assertEqual(od1, od2)
+        pairs = pairs[2:] + pairs[:2]
+        od2 = RankedDict(pairs)
+        # different order implies inequality
+        self.assertNotEqual(od1, od2)
+        # comparison to regular dict is not order sensitive
+        self.assertEqual(od1, dict(od2))
+        self.assertEqual(dict(od2), od1)
+        # different length implied inequality
+        self.assertNotEqual(od1, RankedDict(pairs[:-1]))
+
+    def test_copying(self):
+        # Check that ranked dicts are copyable, deepcopyable, picklable,
+        # and have a repr/eval round-trip
+        pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
+        od = RankedDict(pairs)
+        update_test = RankedDict()
+        update_test.update(od)
+        for i, dup in enumerate([od.copy(), copy.copy(od), copy.deepcopy(od),
+                                 pickle.loads(pickle.dumps(od, 0)),
+                                 pickle.loads(pickle.dumps(od, 1)),
+                                 pickle.loads(pickle.dumps(od, 2)),
+                                 pickle.loads(pickle.dumps(od, -1)),
+                                 eval(repr(od)), update_test,
+                                 RankedDict(od), ]):
+            self.assertTrue(dup is not od)
+            self.assertEqual(dup, od)
+            self.assertEqual(list(dup.items()), list(od.items()))
+            self.assertEqual(len(dup), len(od))
+            self.assertEqual(type(dup), type(od))
+
+    def test_yaml_linkage(self):
+        # Verify that __reduce__ is setup in a way that supports PyYAML's
+        # dump() feature.
+        # In yaml, lists are native but tuples are not.
+        pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
+        od = RankedDict(pairs)
+        # yaml.dump(od) -->
+        # '!!python/object/apply:__main__.RankedDict\n- - [a, 1]\n  - [b, 2]\n'
+        self.assertTrue(all(type(pair) == list for pair in od.__reduce__()[1]))
+
+    def test_repr(self):
+        od = RankedDict([('c', 1), ('b', 2), ('a', 3)])
+        if not PY3:
+            self.assertEqual(
+                repr(od),
+                "RankedDict([('c', 1), ('b', 2), ('a', 3)])")
+        else:
+            self.assertEqual(
+                repr(od),
+                "RankedDict([('c', 1), ('b', 2), ('a', 3)])")
+        self.assertEqual(eval(repr(od)), od)
+        self.assertEqual(repr(RankedDict()), "RankedDict()")
+
+    def test_repr_recursive(self):
+        # See issue #9826
+        od = RankedDict.fromkeys('abc')
+        od['x'] = od
+        if not PY3:
+            self.assertEqual(repr(od),
+                             ("RankedDict([('a', None), ('b', None), "
+                              "('c', None), ('x', ...)])"))
+        else:
+            self.assertEqual(repr(od),
+                             ("RankedDict([('a', None), ('b', None), "
+                              "('c', None), ('x', ...)])"))
+
+    def test_setdefault(self):
+        pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
+        shuffle(pairs)
+        od = RankedDict(pairs)
+        pair_order = list(od.items())
+        self.assertEqual(od.setdefault('a', 10), 3)
+        # make sure order didn't change
+        self.assertEqual(list(od.items()), pair_order)
+        self.assertEqual(od.setdefault('x', 10), 10)
+        # make sure 'x' is added to the end
+        self.assertEqual(list(od.items())[-1], ('x', 10))
+
+        # make sure setdefault still works when __missing__ is defined
+        class Missing(RankedDict):
+            def __missing__(self, key):
+                return 0
+        self.assertEqual(Missing().setdefault(5, 9), 9)
+
+    def test_reinsert(self):
+        # Given insert a, insert b, delete a, re-insert a,
+        # verify that a is now later than b.
+        od = RankedDict()
+        od['a'] = 1
+        od['b'] = 2
+        del od['a']
+        od['a'] = 1
+        self.assertEqual(list(od.items()), [('b', 2), ('a', 1)])
+
+    def test_move_to_end(self):
+        od = RankedDict.fromkeys('abcde')
+        self.assertEqual(list(od), list('abcde'))
+        od.move_to_end('c')
+        self.assertEqual(list(od), list('abdec'))
+        od.move_to_end('c', 0)
+        self.assertEqual(list(od), list('cabde'))
+        od.move_to_end('c', 0)
+        self.assertEqual(list(od), list('cabde'))
+        od.move_to_end('e')
+        self.assertEqual(list(od), list('cabde'))
+        with self.assertRaises(KeyError):
+            od.move_to_end('x')
+
+    @unittest.skipIf('PyPy' in sys.version, 'sys.getsizeof not supported')
+    def test_sizeof(self):
+        # Wimpy test: Just verify the reported size is larger than a regular
+        # dict
+        d = dict(a=1)
+        od = RankedDict(**d)
+        self.assertGreater(sys.getsizeof(od), sys.getsizeof(d))
 
 
 class TestPascalSet(unittest.TestCase):
