4b5b7262c3541d3789150ca0ae9eb9b3fc1c9880
Medardo Rodriguez <med@merchise.org>
2016-09-16 09:33:58 -0400

    Move on deprecation 'xoutil.types' stuffs to 'future'.

--- xoutil/future/types.py
+++ xoutil/future/types.py
@@ -289,4 +289,418 @@ try:
 except ImportError:
     from xoutil.eight._types import _calculate_meta    # noqa
 
+if __name__ == 'xoutil.types':
+    from xoutil.deprecation import deprecated
+    from xoutil import Unset as _unset
+    from collections import Mapping
+    from xoutil.eight import force_type as type_coerce
+
+    # TODO: @manu, consider for future types
+    from re import compile as _regex_compile
+    RegexPattern = type(_regex_compile(''))
+    del _regex_compile
+
+    # TODO: used internally in this module
+    class mro_dict(Mapping):
+        '''Utility behaving like a read-only dict of `target` MRO attributes.
+
+        For example::
+
+          >>> class A(object):
+          ...     x = 12
+          ...     y = 34
+
+          >>> class B(A):
+          ...     y = 56
+          ...     z = 78
+
+          >>> d = mro_dict(B)
+
+          >>> d['x']
+          12
+
+          >>> d['y']
+          56
+
+          >>> d['z']
+          78
+
+        '''
+        # TODO: What is the application for this utility?
+        __slots__ = ('_probes', '_keys')
+
+        def __init__(self, target):
+            from xoutil.inspect import _static_getmro
+            type_ = type_coerce(target)
+            target_mro = _static_getmro(type_)
+            self._probes = tuple(c.__dict__ for c in target_mro)
+            self._keys = set()
+
+        def __getitem__(self, name):
+            from xoutil.objects import get_first_of
+            result = get_first_of(self._probes, name, default=_unset)
+            if result is not _unset:
+                return result
+            else:
+                raise KeyError(name)
+
+        def __iter__(self):
+            if not self._keys:
+                self._settle_keys()
+            return iter(self._keys)
+
+        def __len__(self):
+            if not self._keys:
+                self._settle_keys()
+            return len(self._keys)
+
+        def _settle_keys(self):
+            for probe in self._probes:
+                for key in probe:
+                    if key not in self._keys:
+                        self._keys.add(key)
+
+    # TODO: used internally in this module
+    def mro_get_value_list(cls, name):
+        '''Return a list with all `cls` class attributes in MRO.'''
+        from xoutil.inspect import _static_getmro
+        mro = _static_getmro(type_coerce(cls))
+        return [t.__dict__[name] for t in mro if name in t.__dict__]
+
+    # TODO: not further use
+    def mro_get_full_mapping(cls, name):
+        '''Return a dictionary with all items from `cls` in MRO.
+
+        All values corresponding to `name` must be valid mappings.
+
+        '''
+        aux = mro_get_value_list(cls, name)
+        count = len(aux)
+        if count == 0:
+            return {}
+        elif count == 1:
+            return aux[0]
+        else:
+            res = {}
+            for m in aux:
+                for key in m:
+                    if key not in res:
+                        res[key] = m[key]
+            return res
+
+    # TODO: Many of is_*method methods here are needed to be compared against
+    # the standard lib's module inspect versions.  If they behave the same,
+    # these should be deprecated in favor of the standards.
+
+    # TODO: use in `xoutil.json.JSONEncoder` was replaced by
+    # ``isinstance(maybe, Iterable)``, so now is used only internally.
+    def is_iterable(maybe):
+        '''Returns True if `maybe` is an iterable object (e.g. implements the
+        `__iter__` method):
+
+        ::
+
+            >>> is_iterable('all strings are iterable')
+            True
+
+            # Numbers are not
+            >>> is_iterable(1)
+            False
+
+            >>> from xoutil.eight import range
+            >>> is_iterable(range(1))
+            True
+
+            >>> is_iterable({})
+            True
+
+            >>> is_iterable(tuple())
+            True
+
+            >>> is_iterable(set())
+            True
+
+        '''
+        try:
+            iter(maybe)
+        except:
+            return False
+        else:
+            return True
+
+    # TODO: @manu, external uses to be upgraded:
+    #
+    # - `odoo.addons.mail.xopgi.index`: can be replaced by a local function
+    #   ``isinstance(maybe, (tuple, list, set))`` or by
+    #   `xoutil.cl.simple.logic_iterable_coerce`.
+    #
+    # - `xopgi.account.xopgi.xopgi_proper_currency.move`: ibidem
+    #
+    # - `xopgi.base.xopgi.xopgi_recurrence.models.recurrent_model`: two uses,
+    #   the first is similar to above examples, second use the concept of
+    #   arrays (mappings are collections but not arrays) and maybe must
+    #   consider to move this function to new `xoutil.future.collections` and
+    #   use it from there.  @med, consider `xoutil.cl.simple.array_coerce`.
+    #
+    # - `xopgi.hr.xopgi.xopgi_hr_job_assets.hr_job`: ibidem to first.
+    #
+    # - `xopgi.hr_contract.xopgi.xopgi_hr_contract.hr_contract`: ibidem.
+    #
+    # - `xequiotl.rstmodel`: ibidem.
+    #
+    # - `xoeuf.cli.mailgate`: ibidem.
+    #
+    # - `xoeuf.cli.secure`: ibidem.
+    #
+    # - `xoeuf.osv.model_extensions`: ibidem.
+    #
+    # - `xoeuf.osv.writers`: ibidem.
+    #
+    # - `xotl.simple.event`: ibidem.
+    #
+    # - `xoutil.tests.test_types`: reconfigure all tests after upgrading all
+    #   uses.
+    #
+    # - `xoutil.cpystack`: migrated to use new
+    #   `xoutil.cl.simple.force_sequence_coerce`.
+    #
+    # - `xoutil.fs`: migrated to use `xoutil.cl.simple.force_iterable_coerce`.
+    #
+    # - `xoutil.iterators`: replaced by ``isinstance(fill, Iterable)``.
+    #
+    # - `xoutil.objects`: replaced by
+    #   `xoutil.cl.simple.logic_collection_coerce` or
+    #   `xoutil.cl.simple.logic_iterable_coerce`.
+    def is_collection(maybe):
+        '''Test `maybe` to see if it is a tuple, a list, a set or a generator
+        function.
+
+        It returns False for dictionaries and strings::
+
+            >>> is_collection('all strings are iterable')
+            False
+
+            # Numbers are not
+            >>> is_collection(1)
+            False
+
+            >>> from xoutil.eight import range
+            >>> is_collection(range(1))
+            True
+
+            >>> is_collection({})
+            False
+
+            >>> is_collection(tuple())
+            True
+
+            >>> is_collection(set())
+            True
+
+            >>> is_collection(a for a in range(100))
+            True
+
+        .. versionchanged:: 1.5.5 UserList are collections.
+
+        '''
+        from xoutil.cl.simple import logic_collection_coerce, nil
+        return logic_collection_coerce(maybe) is not nil
+
+    # TODO: There was only one reference to this in `xoutil.objects`, see how
+    # was replaced in the new body of this function
+    def is_mapping(maybe):
+        '''Test `maybe` to see if it is a valid mapping.'''
+        from xoutil.future.collections import Mapping
+        return isinstance(obj, Mapping)
+
+    # only referenced locally by `is_scalar`.
+    def is_string_like(maybe):
+        '''Returns True if `maybe` acts like a string'''
+        try:
+            maybe + ""
+        except TypeError:
+            return False
+        else:
+            return True
+
+    # TODO: @manu, @med, review external references to this function, and
+    # remove them:
+    #
+    # - `xoonko.core.management.profiles.loaders.models_loader`
+    #
+    # - `xoonko.core.management.profiles.producers.json_producer`
+    #
+    # - `xoutil.iterators`
+    def is_scalar(maybe):
+        '''Returns True if `maybe` is a string, an int, or some other scalar type
+        (i.e not an iterable.)
+
+        '''
+        return is_string_like(maybe) or not is_iterable(maybe)
+
+    # TODO: @manu, @med, review external references to this function, and
+    # remove them:
+    #
+    # - `xoutil.objects`: only a reference in a comment, must be removed.
+    #
+    # - `xoutil.records`: fixed.
+    def is_staticmethod(desc, name=_unset):
+        '''Returns true if a `method` is a static method.
+
+        This function takes the same arguments as :func:`is_classmethod`.
+
+        '''
+        if name:
+            desc = mro_dict(desc).get(name, None)
+        return isinstance(desc, staticmethod)
+
+    # TODO: @manu, @med, review external references to this function, and
+    # remove them:
+    #
+    # - `xoutil.objects`: only a reference in a comment, must be removed.
+    def is_classmethod(desc, name=_unset):
+        '''Returns true if a `method` is a class method.
+
+        :param desc: This may be the method descriptor or the class that holds
+               the method, in the second case you must provide the `name` of
+               the method.
+
+               .. note::
+
+                  Notice that in the first case what is needed is the **method
+                  descriptor**, i.e, taken from the class' `__dict__`
+                  attribute. If instead you pass something like
+                  ``cls.methodname``, this method will return False whilst
+                  :func:`is_instancemethod` will return True.
+
+        :param name: The name of the method, if the first argument is the
+               class.
+
+        '''
+        if name:
+            desc = mro_dict(desc).get(name, None)
+        return isinstance(desc, classmethod)
+
+    # not used outside this module.
+    def is_instancemethod(desc, name=_unset):
+        '''Returns true if a given `method` is neither a static method nor a class
+        method.
+
+        This function takes the same arguments as :func:`is_classmethod`.
+
+        '''
+        if name:
+            desc = mro_dict(desc).get(name, None)
+        return isinstance(desc, FunctionType)
+
+    # not used outside this module.
+    def is_slotwrapper(desc, name=_unset):
+        '''Returns True if a given `method` is a slot wrapper (i.e. a method that
+        is builtin in the `object` base class).
+
+        This function takes the same arguments as :func:`is_classmethod`.
+
+        '''
+        if name:
+            desc = mro_dict(desc).get(name, None)
+        return isinstance(desc, SlotWrapperType)
+
+    # not used outside this module.
+    def is_module(maybe):
+        '''Returns True if `maybe` is a module.'''
+        return isinstance(maybe, ModuleType)
+
+    # TODO: @manu, @med, review external references to this function, and
+    # remove them:
+    #
+    # - `xoutil.objects.smart_copy`.
+    @deprecated(type(_unset), 'This is only used in `~objects.smart_copy`.')
+    class Required(object):
+        '''A type for required fields in scenarios where a default is not
+        possible.
+
+        '''
+        def __init__(self, *args, **kwargs):
+            pass
+
+    # TODO: @manu, @med, review external references to this function, and
+    # remove them:
+    #
+    # - `xopgi_recurrence.models.recurrent_model`: sentence
+    #   ``are_instances(leftres, rightres, tuple)`` must be replaced by
+    #   ``isinstance(leftres, tuple) and isinstance(rightres, tuple)``.
+    #
+    # - `xoutil.objects`
+    #
+    # Real "Py4k" signature ``are_instances(*subjects, types)``.
+    def are_instances(*args):
+        '''Return True if every `subject` is an instance of (any) `types`.
+
+        :param subjects: All but last positional arguments.  Are the objects
+            required to be instances of `types`.
+
+        :param types: The last positional argument.  Either a single type or a
+           sequence of types.  This must meet the conditions on the last
+           argument of `isinstance`:func:.
+
+        :returns: True or False.  True if for every `subject`,
+           ``isinstance(subject, types)`` is True.  Otherwise, False.
+
+        If no `subjects` are provided return True::
+
+            >>> are_instances(int)
+            True
+
+        .. seealso:: The function :func:`no_instances` allows to test for
+                     subjects not being instances of types.
+
+        '''
+        if not args:
+            raise TypeError('are_instances requires at least one argument')
+        subjects, types = args[:-1], args[-1]
+        if not subjects:
+            isinstance(None, types)   # HACK: always validate `types`.
+        return all(isinstance(subject, types) for subject in subjects)
+
+    # TODO: @manu, @med, review external references to this function, and
+    # remove them:
+    #
+    # - `xoutil.objects`
+    #
+    # Real Py4k signature ``are_instances(*subjects, types)``self.
+    def no_instances(*args):
+        '''Return True if every `subject` is **not** an instance of (neither)
+        `types`.
+
+        :param subjects: All but last positional arguments.  Are the objects
+               required not to be instances of `types`.
+
+        :param types: The last positional argument.  Either a single type or a
+               sequence of types.  This must meet the conditions on the last
+               argument of `isinstance`:func:.
+
+        :returns: True or False.  True if for every `subject`,
+                  ``isinstance(subject, types)`` is False.  Otherwise, False.
+
+        If no `subjects` are provided return True::
+
+            >>> no_instances(int)
+            True
+
+        .. note:: This is not the same as ``not are_instances(...)``.
+
+           This function requires that *no* subject is an instance of `types`.
+           Negating :func:`are_instances` would be True if *any* subject is
+           not an instance of `types`.
+
+        '''
+        if not args:
+            raise TypeError('no_instances requires at least an argument')
+        subjects, types = args[:-1], args[-1]
+        if not subjects:
+            isinstance(None, types)   # HACK: always validate `types`.
+        return all(not isinstance(subject, types) for subject in subjects)
+
+    del deprecated
+
 del _py2, _py34
