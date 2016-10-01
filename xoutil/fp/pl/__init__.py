b7a1767a33d57b8c9a2f360d760c3d96fe5eb7e3
Medardo Rodriguez <med@merchise.org>
2016-10-01 15:40:59 -0400

    Start "predicate Logic" package.

---
+++ xoutil/fp/pl/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# xoutil.fp.pl
+# ---------------------------------------------------------------------
+# Copyright (c) 2016 Merchise and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2016-10-01
+
+'''Predicate Logic.
+
+.. versionadded:: 1.7.2
+
+'''
