dbe69ba80cc5b8643d07997e4e989eb5d11b1020
Manuel Vázquez Acosta <manu@merchise.org>
2017-02-08 10:38:07 -0500

    Mark next release as 1.7.3.

--- xoutil/release.py
+++ xoutil/release.py
@@ -20,7 +20,7 @@ from __future__ import (division as _py3_division,
                         unicode_literals as _py3_unicode,
                         absolute_import as _py3_abs_imports)
 
-VERSION = '1.7.2'
+VERSION = '1.7.3'
 
 
 def dev_tag_installed():
