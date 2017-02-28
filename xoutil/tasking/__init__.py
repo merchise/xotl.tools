7e6afa31b40abbb2cdea547e928fdc9904093cc7
Medardo Rodriguez <med@merchise.org>
2017-02-28 09:33:17 -0500

    Add 'AutoLocal' class to 'xoutil.tasking' module.
    - Its objetive is to create thread-safe local data in one shoot and delete
      automatically the imported class in your module.

--- xoutil/tasking/__init__.py
+++ xoutil/tasking/__init__.py
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------
 # xoutil.tasking
 # ---------------------------------------------------------------------
-# Copyright (c) 2015-2016 Merchise and Contributors
+# Copyright (c) 2015-2017 Merchise Autrement [~º/~] and Contributors
 # All rights reserved.
 #
 # This is free software; you can redistribute it and/or modify it under the
@@ -20,11 +20,10 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-
-# TODO: Next must be implemented using `xoutil.api` mechanisms for fixing
-# correct driver for a concept, in this case "thread-local data".
 import sys
 
+# TODO: Must be implemented using `xoutil.api` mechanisms for correct driver
+# determination, in this case "thread-local data".
 if 'greenlet' in sys.modules:
     from ._greenlet_local import local    # noqa
 else:
@@ -34,3 +33,27 @@ else:
         from dummy_threading import local    # noqa
 
 del sys
+
+
+class AutoLocal(local):
+    '''Initialize thread-safe local data in one shoot.
+
+    Typical use is::
+
+        >>> from xoutil.tasking import AutoLocal
+        >>> context = AutoLocal(cause=None, traceback=None)
+
+    When at least one attribute is given, ``del AutoLocal`` it's executed
+    automatically avoiding this common statement at the end of your module.
+
+    '''
+    def __init__(self, **attrs):
+        import sys
+        super(AutoLocal, self).__init__()
+        for attr in attrs:
+            setattr(self, attr, attrs[attr])
+        if attrs:
+            g = sys._getframe(1).f_globals
+            tname = type(self).__name__
+            if tname in g:
+                del g[tname]
