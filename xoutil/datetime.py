991d61afc57cc97445eb03ddd43802b6ae552629
Manuel Vázquez Acosta <manu@merchise.org>
2017-08-30 23:41:12 -0400

    Fix closure property of TimeSpan intersection.
    It was intended to be (kind of) a closure in the sense that it would be
    operationally indistinguishable that intersection of disjoint sets return a
    special type of set.

--- xoutil/datetime.py
+++ xoutil/datetime.py
@@ -639,7 +639,7 @@ class TimeSpan(object):
                 end = None
             return type(self)(start, end)
         else:
-            return None
+            return EmptyTimeSpan
     __mul__ = __and__
 
     def intersection(self, *others):
