41d181691d4566ec48612c63e8a76ebcd2131bb5
Manuel Vázquez Acosta <manu@merchise.org>
2017-09-14 15:29:18 -0400

    Add support to create quantities with units of measurement.
    This implementation comes from mercurio/xhg.autrement.core@master.  However,
    after the terminology found in [1] we have changed some terms.
    
    [1] https://en.wikipedia.org/wiki/Dimensional_analysis
    
    Also we removed some limitations of the original implementation.  We can now
    define ``1/L`` and ``L**-n``.  We don't support arbitrary ``n/L``, just n=1.

---
+++ tests/test_dim.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ---------------------------------------------------------------------
+# test_units
+# ---------------------------------------------------------------------
+# Copyright (c) 2015-2017 Merchise Autrement and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2015-05-12
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+import pytest
+from hypothesis import given, strategies as s
+
+from xoutil.dim.meta import (
+    Signature,
+    Quantity,
+    QuantityType,
+    Scalar,
+    UNIT,
+)
+
+
+def test_usage():
+    @QuantityType.new
+    class L(object):
+        metre = UNIT
+        kilometre = 1000 * metre
+
+    @QuantityType.new
+    class T(object):
+        second = UNIT
+
+    m, km = L.metre, L.kilometre
+    s = T.second
+
+    assert isinstance(m, L)
+    assert not isinstance(1, L)
+    assert isinstance(s, T)
+
+    assert -km == -1 * km == -(1000 * m)
+    assert +km == km
+
+    Speed = L/T
+
+    assert isinstance(m / s, Speed)
+    assert not isinstance(m, T)
+
+    assert hasattr(L / T, 'metre_per_second')
+
+    assert 10 * km == 10000 * m
+    assert m < km
+
+    with pytest.raises(TypeError):
+        10 + 10 * km
+
+    with pytest.raises(TypeError):
+        m + s
+
+    assert Speed.metre_per_second == m / s
+
+    Acceleration = L / (T * T)
+    assert hasattr(Acceleration, 'metre_per_second_squared')
+    # however:
+    Not_Acceleration = L / T * T
+    assert hasattr(Not_Acceleration, 'metre_per_second_second')
+    assert L == Not_Acceleration, 'It is the same as Length'
+
+
+def test_effort():
+    @QuantityType.new
+    class Workforce(object):
+        men = UNIT
+
+    @QuantityType.new
+    class Time(object):
+        second = UNIT
+
+    class Effort(Workforce * Time):
+        # Since the canonical unit of a composed quantity type is built from
+        # the canonical units of the operands, but the true "canonical type"
+        # of effort is usually men-hour we re-introduce it.
+        men_hour = 60
+
+    assert Effort.men_hour > Effort.men_second
+
+
+def test_scalar_downgrade():
+    from xoutil.dim.app.standard import L
+    km = L.km
+    assert not isinstance(km / km, Quantity)
+    assert km / km == 1
+    assert not isinstance(km // km, Quantity)
+    assert km // km == 1
+
+    assert not isinstance(1 / km * km, Quantity)
+    assert float(1 / km * km)
+
+
+def test_natural_downgrade():
+    from xoutil.dim.app.standard import L
+    km, cm = L.km, L.cm
+    assert float(km) == 1000
+    assert int(cm) == 0
+
+
+def test_decimals():
+    import decimal
+    from xoutil.dim.app.standard import m
+    with pytest.raises(TypeError):
+        third = decimal.Decimal('0.33') * m
+        assert third < m
+
+
+def test_signatures():
+    distance = Signature('m')
+    time = Signature('s')
+    freq = 1 / time
+    speed = distance / time
+    acceleration = speed / time
+    assert acceleration == distance / (time * Signature('s'))
+    assert speed == distance * freq
+    assert speed**3 == speed * speed * speed
+    assert speed**0 == Signature() == speed / speed
+    assert speed**-3 == 1 / (speed**3)
+
+
+def test_quantity_math():
+    metre = m = Quantity(1, Signature('m'))
+    second = Quantity(1, Signature('s'))
+
+    with pytest.raises(TypeError):
+        metre < second
+
+    assert metre < 2 * metre < (metre + 2 * metre)
+    assert metre * metre == Quantity(1, Signature('mm'))
+    assert metre / second == Quantity(1, Signature('m', 's'))
+    assert metre * metre * metre == metre**3
+    assert 1/(metre * metre * metre) == metre**-3
+
+    assert 1000 * m % 3 == 1 * m
+    assert 5 % (2 * m) == 1 / m
+    assert 5 / (2 * m) == 2.5 / m
+
+    with pytest.raises(TypeError):
+        5 ** (2*m)
+
+
+def test_quantity_type_definitions():
+    from xoutil.dim.app.standard import Length as Distance, Time
+    assert isinstance(Distance, QuantityType)
+    assert isinstance(Time, QuantityType)
+    assert isinstance(Distance / Time, QuantityType)
+    assert isinstance(Distance**2, QuantityType)
+    assert Distance * Distance == Distance**2
+
+    assert Time / Time == Scalar
+    assert Time / Time * Time == Time
+
+    with pytest.raises(TypeError):
+        Distance + Time
+
+    with pytest.raises(TypeError):
+        Distance - Time
+
+    assert Distance**1 is Distance
+
+    with pytest.raises(TypeError):
+        Distance**1.2
+
+    with pytest.raises(TypeError):
+        Distance**0
+
+    assert Distance**-1 == 1 / Distance
+
+    with pytest.raises(TypeError):
+        2 / Distance
+
+    with pytest.raises(TypeError):
+        2 * Distance
+
+
+@given(s.floats(allow_nan=False) | s.integers())
+def test_any_magnitude(m):
+    from xoutil.dim.app.standard import L
+    assert float(m * L.metre) == float(m)
+
+
+@given(s.floats(allow_nan=False, allow_infinity=False) | s.integers())
+def test_any_magnitude_noinf(m):
+    from xoutil.dim.app.standard import L
+    from math import ceil, floor
+    from six import integer_types
+    Int = integer_types[-1]
+    q = m * L.metre
+    for f in (Int, float, abs, round, ceil, floor):
+        assert f(q) == f(m)
---
+++ xoutil/dim/app/standard.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# standard
+# ---------------------------------------------------------------------
+# Copyright (c) 2017 Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the LICENCE attached (see LICENCE file) in the distribution
+# package.
+#
+# Created on 2017-09-14
+
+'''The standard physical quantities.
+
+'''
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+from ..meta import (
+    QuantityType,
+    UNIT,
+)
+
+
+def kilo(v):
+    return 1000 * v
+
+
+def centi(v):
+    return v / 100
+
+
+def milli(v):
+    return v / 1000
+
+
+def micro(v):
+    return v / 1000000
+
+
+def nano(v):
+    return v / (10**9)
+
+
+@QuantityType.new
+class Length(object):
+    'Length'
+    metre = UNIT
+    kilometer = km = kilo(metre)
+    centimeter = cm = centi(metre)
+    millimeter = mm = milli(metre)
+    nanometer = nm = nano(metre)
+
+
+metre = m = Length.m = Length.metre
+L = Length
+
+
+@QuantityType.new
+class Time(object):
+    'Time'
+    second = UNIT
+    millisecond = milli(second)
+    nanosecond = nano(second)
+    minute = second * 60
+    hour = minute * 60
+
+
+second = s = Time.s = Time.second
+T = Time
+
+
+@QuantityType.new
+class Mass(object):
+    'Mass'
+    kilogram = UNIT
+    gram = kilogram / 1000
+
+
+kilogram = kg = Mass.kg = Mass.kilogram
+M = Mass
+
+
+@QuantityType.new
+class ElectricCurrent(object):
+    'Electric Current'
+    ampere = UNIT
+    milliampere = milli(ampere)
+
+
+A = ampere = ElectricCurrent.A = ElectricCurrent.ampere
+I = ElectricCurrent
+
+
+@QuantityType.new
+class Temperature(object):
+    'Thermodynamic temperature'
+    kelvin = UNIT
+    C = celcius = kelvin - 273.15
+    F = fahrenheit = kelvin * (9 / 5) - 459.67
+
+
+K = kelvin = Temperature.K = Temperature.kelvin
+C = celcius = Temperature.C
+O = Temperature   # The actual symbol would be the capital letter Theta: Θ
+
+
+@QuantityType.new
+class Substance(object):
+    mole = UNIT
+
+
+mol = Substance.mol = Substance.mole
+N = Substance
+
+
+@QuantityType.new
+class Luminosity(object):
+    'Luminous intensity'
+    candela = UNIT
+
+
+J = Luminosity
+
+
+# Derived quantities
+Area = L**2
+Volume = L**3
+Frequency = T**-1
+Force = L * M * T**-2
+Presure = L**-1 * M * T**-2
+Velocity = L * T**-1
+Acceleration = L * T**-2
