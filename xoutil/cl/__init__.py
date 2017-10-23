7d6f3240c285ef06067c4ed3f65a362a807ba71b
Medardo Rodriguez <med@merchise.org>
2017-10-23 17:38:00 -0400

    Fix minor issues in 'xoutil.cl'.

--- xoutil/cl/__init__.py
+++ xoutil/cl/__init__.py
@@ -41,7 +41,6 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
-
 import re
 from xoutil.eight.abc import ABCMeta
 from xoutil.eight.meta import metaclass
