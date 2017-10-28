12d55d0eb7eb99d1895c0efb74182aaf7dfd3e90
Manuel Vázquez Acosta <manuel@merchise.org>
2017-10-28 15:56:20 -0400

    Correct version.

--- xoutil/release.py
+++ xoutil/release.py
@@ -18,7 +18,7 @@ from __future__ import (division as _py3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_imports)
 
-VERSION = '1.9.0'
+VERSION = '1.8.0'
 
 
 def dev_tag_installed():
