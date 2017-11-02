a77aed2e177c9d071e2e683039d871cc3a7d4f31
Medardo Rodriguez <med@merchise.org>
2017-11-02 13:36:56 -0400

    Fix minor issues in 'eight.queue' module documentation.

--- xoutil/eight/queue.py
+++ xoutil/eight/queue.py
@@ -12,7 +12,7 @@
 #
 # Created on 2015-11-19
 
-'''Standardization of Queque Processing.
+'''Standardization of Queue Processing.
 
 In Python 2, the Queue processing module (A multi-producer, multi-consumer
 queue) has the name with initial capital letter, nevertheless in Python 3 the
@@ -21,12 +21,12 @@ name is all in lower-case.
 This is a compatibility module, importing symbols from this will be the same
 in both versions of Python.
 
-Im Python 3 module there are two symbols imported from ``heapq`` module, those
+In Python 3 module there are two symbols imported from ``heapq`` module, those
 are imported here for full compatibility.
 
 '''
 
-from __future__ import (division as _py3_division,
+from __future__ import (division as _Ty3_division,
                         print_function as _py3_print,
                         absolute_import as _py3_abs_import)
 
