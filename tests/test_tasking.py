81b16339236fe98b8350b75355bb7e7830e86006
Manuel Vázquez Acosta <manuel@merchise.org>
2017-11-20 19:22:38 -0500

    Add basic tests for `dispatch`.
    This will also test the behavior of dispatcher and _autoretry.  In fact, it
    shows some bugs.

---
+++ tests/test_tasking.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+from __future__ import (division as _py3_division,
+                        print_function as _py3_print,
+                        absolute_import as _py3_abs_import)
+
+import pytest
+from xoutil.tasking import dispatch
+
+
+class FailingMock(object):
+    def __init__(self, start=0, threshold=5, sleeper=0):
+        self.start = start
+        self.threshold = threshold
+        self.sleeper = sleeper
+
+    def __call__(self, incr=1):
+        if self.sleeper:
+            import time
+            time.sleep(self.sleeper)
+        self.start += incr
+        if self.start < self.threshold:
+            raise ValueError(self.start)
+        else:
+            return self.start
+
+
+def test_retrying_max_tries_not_enough_tries():
+    fn = FailingMock(threshold=1000000, sleeper=0.5)
+    with pytest.raises(ValueError):
+        dispatch(fn, max_tries=3)
+
+
+def test_retrying_max_tries_enough_tries():
+    fn = FailingMock()
+    assert dispatch(fn, max_tries=10) == 5
+
+
+def test_retrying_max_time_runout():
+    fn = FailingMock(threshold=1000000, sleeper=0.2)
+    with pytest.raises(ValueError):
+        dispatch(fn, max_time=0.3)
+
+
+def test_retrying_max_time():
+    fn = FailingMock(sleeper=0.2)
+    assert dispatch(fn, max_time=0.2 * 5) == 5
+
+
+def test_signature():
+    fn = FailingMock()
+    with pytest.raises(TypeError):
+        assert dispatch(fn, (), {}, 5)
+    with pytest.raises(TypeError):
+        assert dispatch(fn, (), {}, x=5)
