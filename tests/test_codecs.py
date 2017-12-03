5e2299a5ec78f1dc3ddc592d7e41207a7e73b37e
Manuel Vázquez Acosta <manuel@merchise.org>
2017-12-03 09:52:44 -0500

    Test type of result of safe_encode and safe_decode.

--- tests/test_codecs.py
+++ tests/test_codecs.py
@@ -25,3 +25,19 @@ def test_safe_decode_dont_fail_uppon_invalid_encoding(s):
 def test_safe_encode_dont_fail_uppon_invalid_encoding(s):
     from xoutil.future.codecs import safe_encode
     assert safe_encode(s, 'i-dont-exist') == safe_encode(s)
+
+
+@given(text())
+def test_safe_encode_yields_bytes(s):
+    from xoutil.future.codecs import safe_encode
+    assert isinstance(safe_encode(s), bytes)
+
+
+@given(binary())
+def test_safe_decode_yields_unicode(s):
+    try:
+        Text = unicode
+    except NameError:
+        Text = str  # Python 3
+    from xoutil.future.codecs import safe_decode
+    assert isinstance(safe_decode(s), Text)
