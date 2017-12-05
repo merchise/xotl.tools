e51f5a0c4dc68d10cd5e283761dc32be81f77a56
Medardo Rodriguez <med@merchise.org>
2017-12-05 13:04:48 -0500

    Add 'urllib' compatibility module.

---
+++ xoutil/eight/urllib/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# Copyright (c) Merchise Autrement [~º/~] and Contributors
+# All rights reserved.
+#
+# This is free software; you can do what the LICENCE file allows you to.
+#
+
+'''Compatibility to Python's `urllib`:mod: standard module.'''
