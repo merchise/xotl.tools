cd8529558853133a0cc6ebf5c92411cf2ccfee06
Manuel Vázquez Acosta <manuel@merchise.org>
2018-03-30 16:29:18 -0400

    State explicitly which Python versions we support.

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=[
         'monotonic; python_version<"3.3"',
     ],
