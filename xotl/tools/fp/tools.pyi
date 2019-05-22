4fbe62526846947a9defebfb1137455ba51081ca
Manuel Vázquez Acosta <manuel@merchise.org>
2019-05-22 14:18:27 -0400

    Correct the type hints of 'compose'.
    I expect 'compose' to be used like a function.  So this type hints will work
    for most of the expected usage.

--- xotl/tools/fp/tools.pyi
+++ xotl/tools/fp/tools.pyi
@@ -3,14 +3,28 @@ from typing import Any, Callable, Dict, Type, Union, TypeVar, overload
 A = TypeVar("A")
 B = TypeVar("B")
 C = TypeVar("C")
+Z = TypeVar("Z")
+X = TypeVar("X")
+Y = TypeVar("Y")
 
 def identity(arg: C) -> C: ...
-
-class compose:
-    # I'm putting just the case of simple composition, and the chaining of several
-    # homogeneous functions.  I'm disregarding the full_args and pos_args, because
-    # Callable cannot express that.
-    @overload
-    def __call__(f: Callable[[A], B], g: Callable[[C], A]) -> Callable[[C], B]: ...
-    @overload
-    def __call__(*fn: Callable[[A], A]) -> Callable[[A], A]: ...
+@overload
+def compose(f: Callable[[A], B], g: Callable[[C], A]) -> Callable[[C], B]: ...
+@overload
+def compose(
+    z: Callable[[B], Z], f: Callable[[A], B], g: Callable[[C], A]
+) -> Callable[[C], Z]: ...
+@overload
+def compose(
+    x: Callable[[Z], X], z: Callable[[B], Z], f: Callable[[A], B], g: Callable[[C], A]
+) -> Callable[[C], X]: ...
+@overload
+def compose(
+    y: Callable[[X], Y],
+    x: Callable[[Z], X],
+    z: Callable[[B], Z],
+    f: Callable[[A], B],
+    g: Callable[[C], A],
+) -> Callable[[C], Y]: ...
+@overload
+def compose(*fn: Callable[[A], A]) -> Callable[[A], A]: ...
