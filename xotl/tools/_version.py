811f1c13722c75bd3b9429f4dfc50ece2938b7ce
Manuel Vázquez Acosta <manuel@merchise.org>
2019-05-26 19:59:35 -0400

    Fix tag_prefix.

--- xotl/tools/_version.py
+++ xotl/tools/_version.py
@@ -41,7 +41,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "None"
+    cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "xotl/tools/_version.py"
     cfg.verbose = False
