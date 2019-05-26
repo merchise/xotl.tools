2b415cf6c7c9a90412b45a63f10a433d5b576d65
Manuel Vázquez Acosta <manuel@merchise.org>
2019-05-26 11:06:31 -0400

    Use versioneer to control the package version.

--- xotl/tools/__init__.py
+++ xotl/tools/__init__.py
@@ -6,11 +6,15 @@
 #
 # This is free software; you can do what the LICENCE file allows you to.
 #
-
-'''Collection of disparate utilities.
+"""Collection of disparate utilities.
 
 ``xotl.tools`` is essentially an extension to the Python's standard library,
 it does not make up a full framework, but it's very useful to be used from a
 diversity of scenarios.
 
-'''
+"""
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
