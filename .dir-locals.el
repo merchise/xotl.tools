bfbcd72a44f359640f81daf8fbe639aa767ada14
Manuel Vázquez Acosta <mva.led@gmail.com>
2026-09-22 08:24:17 +0200

    chore: Remove out-dated .dir-locals.el

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((projectile-project-compilation-cmd . "source .envrc; make format")))
-  (python-mode . ((fill-column . 100))))
